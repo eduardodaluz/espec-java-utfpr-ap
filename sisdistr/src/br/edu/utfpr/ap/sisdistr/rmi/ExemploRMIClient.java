63db7ddb12b36da51ef6df87cb986dfa7d049a1b
Eduardo Rodrigues da Luz <eduardo.r.luz@gmail.com>
2011-07-09 12:21:55 -0300

    Alterada alguns comentários da classe

--- sisdistr/src/br/edu/utfpr/ap/sisdistr/rmi/ExemploRMIClient.java
+++ sisdistr/src/br/edu/utfpr/ap/sisdistr/rmi/ExemploRMIClient.java
@@ -1,10 +1,15 @@
 package br.edu.utfpr.ap.sisdistr.rmi;
 
 import java.rmi.registry.*;
-import exemplormi.ExemploInterface; //para descrever a Interface (deve-se copiar para outra máquina, ou seja,
-//"ExemploRMI" refere-se ao diretório e ExemploInterface refere-se ao .class
-// do ExemploInterface). No Netbeans, deve-se inserir a raiz da pasta copiada
-//"ExemploRMI" no item Bibliotecas....
+import exemplormi.ExemploInterface;
+/*
+ * para descrever a Interface
+ * (deve-se copiar para outra máquina, ou seja,
+ * "exemplormi" refere-se ao diretório e ExemploInterface refere-se ao .class do ExemploInterface).
+ * No Netbeans, deve-se inserir a raiz da pasta copiada "ExemploRMI" no item Bibliotecas...
+ * No eclipse se adiciona uma "Class Folder" ou "External Class Folder"...
+ * 
+ */
 
 public class ExemploRMIClient {
 	public static void main(String [] args){
@@ -13,8 +18,10 @@ public class ExemploRMIClient {
 			ExemploInterface stub = (ExemploInterface) registry.lookup("Exemplo");
 			//Procura pelo registro Exemplo para retornar a instancia do stub remoto (ref. obj. rem.)
 			//A definição da classe stub é baixada dinamicamente no cliente se o java > 1.5
-			String resposta = stub.mostraTexto("11:45 em Brasolia!"); //invoca o método remoto
-			System.out.println("Resposta remota: " + resposta);
+			while(true) {
+				String resposta = stub.mostraTexto("Esse programa executou uma operação ilegal e será fechado!"); //invoca o método remoto
+				System.out.println("Resposta remota: " + resposta);
+			}
 		} catch (Exception e) {
 			System.err.println("Client exception: " + e.toString());
 		}
