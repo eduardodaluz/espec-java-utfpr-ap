5c2666f88de31412d0821f7ed199a23990c7a4dc
Eduardo Rodrigues da Luz <eduardo.r.luz@gmail.com>
2011-11-06 18:27:40 -0200

    Criado WebService de Produto

---
+++ BotiqueWeb/src/java/webservices/FornecedorWS.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webservices;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author eduardo
+ */
+@WebService(serviceName = "FornecedorWS")
+public class FornecedorWS {
+
+    /** This is a sample web service operation */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
---
+++ BotiqueWeb/src/java/webservices/ProdutoWS.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webservices;
+
+import controle.ControleProduto;
+import java.util.List;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import modelo.Produto;
+
+/**
+ *
+ * @author eduardo
+ */
+@WebService(serviceName = "ProdutoWS")
+public class ProdutoWS {
+    
+    ControleProduto controlProd;
+    
+    public ProdutoWS() {
+        controlProd = new ControleProduto();
+    }
+
+    @WebMethod(operationName = "inserir")
+    public String inserir(@WebParam(name = "produto") Produto produto) {
+        return controlProd.inserir(produto);
+    }
+    
+    @WebMethod(operationName = "atualizar")
+    public String atualizar(@WebParam(name = "produto") Produto produto) {
+        return controlProd.atualizar(produto);
+    }
+    
+    @WebMethod(operationName = "deletar")
+    public String deletar(@WebParam(name = "id") int id) {
+        return controlProd.deletar(id);
+    }
+    
+    @WebMethod(operationName = "getProduto")
+    public Produto getProduto(@WebParam(name = "id") int id) {
+        return controlProd.getProduto(id);
+    }
+    
+    @WebMethod(operationName = "listaDeProdutos")
+    public List<Produto> listaDeProdutos() {
+        return controlProd.listaDeProdutos();
+    }
+    
+    @WebMethod(operationName = "listaDeProdutosPorDescricao")
+    public List<Produto> listaDeProdutosPorDescricao(
+            @WebParam(name = "descricao") String descricao) {
+        return controlProd.listaDeProdutosPorDescricao(descricao);
+    }
+    
+}
